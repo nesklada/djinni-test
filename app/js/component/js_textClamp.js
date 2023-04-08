224401d2935557a4261910da23edf99066ee9efd
Stan <nesklada.gov@gmail.com>
2023-04-08 22:23:16 +0300

    resize event for 'Show more' toggler

--- app/js/component/js_textClamp.js
+++ app/js/component/js_textClamp.js
@@ -1,7 +1,11 @@
 import createElement from "./config/createElement";
 
-export default function(fromIndex = 0) {
-    const target = 'js_textClamp';
+const target = 'js_textClamp';
+const targetToggler = target + 'Toggler';
+const hide = 'hide';
+let windowWidth = window.innerWidth;
+
+export default function init(fromIndex = 0) {
     const nodes = document.querySelectorAll(`.${target}`);
     
     if(!nodes) {
@@ -11,14 +15,27 @@ export default function(fromIndex = 0) {
     const $targets = Array.prototype.slice.call(nodes, fromIndex ? nodes.length - fromIndex : 0);
 
     $targets.forEach((node) => {
+        const togglerInited = node.parentNode.querySelector('.' + targetToggler);
+        const isOverflow = isTextClamped(node);
 
-        if(!isTextClamped(node)) {
+        if(togglerInited) {
+            
+            if(isOverflow) {
+                togglerInited.classList.remove(hide);
+            } else {
+                togglerInited.classList.add(hide);
+            }
+
+            return
+        }
+
+        if(!isOverflow) {
             return
         }
 
         const $toggler = createElement({
             tag: 'button',
-            classList: ['btn-reset', 'btn-text'],
+            classList: ['btn-reset', 'btn-text', targetToggler],
             text: 'Show more...',
             attrs: {
                 type: 'button'
@@ -32,6 +49,19 @@ export default function(fromIndex = 0) {
             $toggler.remove()
         });
     });
+
+    window.removeEventListener('resize', onResize);
+    window.addEventListener('resize', onResize);
+}
+
+function onResize() {
+    if(windowWidth === window.innerWidth) {
+        return
+    }
+
+    windowWidth = window.innerWidth;
+
+    init();
 }
 
 const isTextClamped = elm => elm.scrollHeight > elm.clientHeight;
